eb00c7a3040e979b9ff0c7c1041facddbcc8983b
Рома Адамович <roman-adamovich@mail.ru>
2023-04-24 18:30:29 +0500

    удалил лишний объект

--- testHackathon_frontEnd/src/components/Main/Main.jsx
+++ testHackathon_frontEnd/src/components/Main/Main.jsx
@@ -40,15 +40,7 @@ const Main = (props) => {
                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                 </svg>
             </div>
-            <Map
-                initialViewState={{
-                    longitude: -100,
-                    latitude: 40,
-                    zoom: 3.5
-                }}
-                style={{width: 600, height: 400}}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            />;
+
         </div>
     )
 }
