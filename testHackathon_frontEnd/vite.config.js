9f3bd5bb248f0f7cf6f6528db93fe4ca527e274e
Рома Адамович <roman-adamovich@mail.ru>
2023-04-24 12:18:44 +0500

    изменил vit config

--- testHackathon_frontEnd/vite.config.js
+++ testHackathon_frontEnd/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': { target: 'http://192.168.215.83:3000' },
+    plugins: [react()],
+    server: {
+        host: '91.142.72.178',
+        port: 5000,
     },
-  },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './test-setup/setup.test.js',
+        testTimeout: 60000,
+        include: ['**/*.{test,spec}.{js,mjs, ts}']
+    }
 })
