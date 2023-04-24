83ce2131c12a86f6c004b45f37a2742cdaf1b737
stalk <stalkeronline2016@mail.ru>
2023-04-24 09:17:16 +0300

    update dockerfile

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -8,14 +8,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { RegistrationModule } from './core/registration/registration.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './core/auth/auth.guard';
-
+import { MongooseConfigService } from './core/config/MongooseConfigService';
+//const uri = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DATABASE_LOCAL_DOMAIN}:${process.env.DATABASE_LOCAL_PORT}`;
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     RegistrationModule,
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useClass: MongooseConfigService,
+    // }),
     MongooseModule.forRoot(
-      `mongodb://${process.env.DATABASE_LOCAL_DOMAIN}:${process.env.DATABASE_LOCAL_PORT}`,
+      `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGODB_URL}`,
       {
         dbName: process.env.DATABASE_NAME,
       },
